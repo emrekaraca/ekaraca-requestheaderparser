e93e6f6d484eb30ac8c5066b34b69248ee2fab91
emrekaraca <creagas@googlemail.com>
2017-01-24 16:53:40 +0100

    fix ip address reader

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var useragent = require('useragent');
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-  var ip = req.ip;
+  var ip = req.ip.replace(/^.*:/, '');
   var lg = req.acceptsLanguages()[0];
   var agent = useragent.parse(req.headers['user-agent']);
   var os = agent.os.toString();
