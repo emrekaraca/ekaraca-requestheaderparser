72cd3569012d6e47530f317571c0398bc9052fbd
emrekaraca <creagas@googlemail.com>
2017-01-24 16:32:14 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var useragent = require('useragent');
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  var ip = req.ip;
+  var lg = req.acceptsLanguages()[0];
+  var agent = useragent.parse(req.headers['user-agent']);
+  var os = agent.os.toString();
+  res.json({'ipaddress': ip, 'language': lg, 'software': os})
+})
+
+app.listen(port);
